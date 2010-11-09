18153646d6e70e5527141d323413eac68990f01b
Alex Crawford <adrofwarc+github@gmail.com>
2010-11-09 14:21:33 -0500

    Added dynamic naming and coloring

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 	$starttime = microtime(true);
 
-	echo '<h2>im the map, im the map, im the map, im the map, IM THE MAP!!!</h2>';
 
 	$username = 'uid=<username>,';
 	$password = '<password>';
+
+	$colorRTPEboard = '#00FFFF';
+	$colorRTP = '#00FF00';
+	$colorEboard = '#0000FF';
 
 	$baseDN = 'dc=csh,dc=rit,dc=edu';
 	$usersDN = 'ou=Users,'.$baseDN;
@@ -20,15 +23,18 @@
 
 
 	# Find all On-Floor members
-	$results = ldap_search($ldap, $usersDN, '(&(onfloor=1)(objectClass=houseMember))', array('cn'));
+	$results = ldap_search($ldap, $usersDN, '(&(onfloor=1)(objectClass=houseMember))', array('nickname', 'roomNumber', 'cn'));
 	$onfloors = ldap_get_entries($ldap, $results);
 
 	# Insert them into the members array
+	$room = 3009;
 	foreach ($onfloors as $person) {
 		if (!isset($person['dn']))
 			continue;
 
 		$members[$person['dn']] = array('name' => $person['cn'][0], 
+		                                'room' => $person['roomNumber'][0],
+										'year' => 0,
 		                                'rtp' => false,
 		                                'eboard' => false);
 	}
@@ -57,27 +63,48 @@
 		$members[$person]['rtp'] = true;
 	}
 
-
-	# Print them out
-	foreach($members as $member) {
-		if ($member['rtp'] and $member['eboard']) {
-			echo '<font color="#00aaaa">';
-		} else if ($member['rtp']) {
-			echo '<font color="#00aa00">';
-		} else if ($member['eboard']) {
-			echo '<font color="#0000aa">';
-		} else {
-			echo '<font>';
-		}
-		echo $member['name'], '</font><br />';
-	}
-	
-
 	# Unbind from LDAP
 	ldap_unbind($ldap);
+
+
+	# Fill the variables with info
+	foreach($members as $member) {
+		$name = 'n'.$member['room'];
+		if ($member['rtp'] and $member['eboard']) {
+			$$name = '<font color="'.$colorRTPEboard.'">';
+		} else if ($member['rtp']) {
+			$$name = '<font color="'.$colorRTP.'">';
+		} else if ($member['eboard']) {
+			$$name = '<font color="'.$colorEboard.'">';
+		} else {
+			$$name = '<font>';
+		}
+		$$name += $member['name'].'</font>';
+		
+		$color = 'b'.$person['room'];
+		$$color = '#FFAAAA';
+	}
+
+	require('map.html');
 
 	$endtime = microtime(true);
 
 	$total = round($endtime - $starttime, 4);
-	echo 'Request took ', $total, ' seconds to complete. To bad LDAP is soooo fucking slow.';
+	echo '<br /><br />Request took ', $total, ' seconds to complete.';
+
+	function printnames($members) {
+		# Print them out
+		foreach($members as $member) {
+			if ($member['rtp'] and $member['eboard']) {
+				echo '<font color="#00aaaa">';
+			} else if ($member['rtp']) {
+				echo '<font color="#00aa00">';
+			} else if ($member['eboard']) {
+				echo '<font color="#0000aa">';
+			} else {
+				echo '<font>';
+			}
+			echo $member['name'], '</font><br />';
+		}
+	}
 ?>
