b8f8d8df5d2bdac0da4eae9ee5d5d435893d8d87
Alex Crawford <adrofwarc+github@gmail.com>
2011-04-17 17:57:14 -0400

    Added support for websockets with SUDS

--- suds.js
+++ suds.js
@@ -1,29 +1,97 @@
-setInterval("refresh()", 15000);
-refresh();
-
-function refresh() {
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", "sudsStatus.php", true);
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			parseStatuses(xmlhttp.responseText);
-		}
-	};
-	xmlhttp.send();
-}
-
-function parseStatuses(str) {
-	var statuses = eval(str);
-
+function parseStatus(stall) {
 	occupied = "rgba(200,100,100,0.8)";
 	free = "rgba(100,200,100,0.8)";
 
-	document.getElementById("sudsSSeast").style.backgroundColor = (statuses[0] == 0) ? free : occupied;
-	document.getElementById("sudsSSwest").style.backgroundColor = (statuses[1] == 0) ? free : occupied;
-	document.getElementById("sudsSNeast").style.backgroundColor = (statuses[2] == 0) ? free : occupied;
-	document.getElementById("sudsSNwest").style.backgroundColor = (statuses[3] == 0) ? free : occupied;
-	document.getElementById("sudsNSeast").style.backgroundColor = (statuses[4] == 0) ? free : occupied;
-	document.getElementById("sudsNSwest").style.backgroundColor = (statuses[5] == 0) ? free : occupied;
-	document.getElementById("sudsNNeast").style.backgroundColor = (statuses[6] == 0) ? free : occupied;
-	document.getElementById("sudsNNwest").style.backgroundColor = (statuses[7] == 0) ? free : occupied;
+	switch (stall.id) {
+		case 1:
+			stallDiv = document.getElementById("sudsSSeast");
+			break;
+		case 2:
+			stallDiv = document.getElementById("sudsSSwest");
+			break;
+		case 3:
+			stallDiv = document.getElementById("sudsSNeast");
+			break;
+		case 4:
+			stallDiv = document.getElementById("sudsSNwest");
+			break;
+		case 5:
+			stallDiv = document.getElementById("sudsNSeast");
+			break;
+		case 6:
+			stallDiv = document.getElementById("sudsNSwest");
+			break;
+		case 7:
+			stallDiv = document.getElementById("sudsNNeast");
+			break;
+		case 8:
+			stallDiv = document.getElementById("sudsNNwest");
+			break;
+		default:
+			return;
+	}
+
+	if (stall.status == 0) {
+		stallDiv.style.backgroundColor = free;
+	} else {
+		stallDiv.style.backgroundColor = occupied;
+	}
 }
+
+function parseStatuses(stalls) {
+	for (stall in stalls.north) {
+		parseStatus(stalls.north[stall]);
+	}
+	for (stall in stalls.south) {
+		parseStatus(stalls.south[stall]);
+	}
+}
+
+
+//Adapted from ComputerScienceHouse/SUDS
+//Originally written by Sean McGary
+window.onload = function() {
+	var socket = new io.Socket(null, {port: 8888, secure: false, rememberTransport: false});
+	socket.connect();
+
+	socket.on('message', function(obj)
+	{
+		console.log(obj);
+		switch(obj.opcode) {
+			case "data_init":
+			case "stall_dump":
+				parseStatuses(obj['stalls']);
+				break;
+			case "update_stall":
+				parseStatus(obj);
+				break;
+			default:
+				break;
+		}
+	});
+
+	socket.on('connect', function()
+	{
+		console.log('connected');
+	});
+
+	socket.on('disconnect', function()
+	{
+		console.log("disconnected");
+	});
+
+	socket.on('reconnect', function()
+	{
+		console.log("reconnected");
+	});
+
+	socket.on('reconnecting', function( nextRetry )
+	{
+		console.log("reconnecting");
+	});
+
+	socket.on('reconnect_failed', function()
+	{
+		console.log("failed to reconnect");
+	});
+};
